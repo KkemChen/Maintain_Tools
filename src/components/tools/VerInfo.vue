f80a829957ca6e6ddf053c9dce1f9a783254bf9b
KkemChen <kkemchen@qq.com>
2023-12-27 17:55:40 +0800

    feat: 添加VerInfo页面

---
+++ src/components/tools/VerInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-table :data="tableData" border style="width: 100%" id="myTable">
+    <el-table-column prop="name" label="Name" :width="columnWidths.date" />
+    <el-table-column prop="date" label="Date" :width="columnWidths.name" />
+    <el-table-column prop="commit" label="Commit" :width="columnWidths.address" />
+  </el-table>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+interface TableDataItem {
+  name: string;
+  date: string;
+  commit: string;
+}
+
+const tableData = ref<TableDataItem[]>([
+  {
+    name: 'StreamServer',
+    date: '2023-12-25',
+    commit: '2147b4ff7d4921b3ce0febed0b6c267f161d6d63',
+  },
+]);
+
+const columnWidths = ref({
+  date: '25%',
+  name: '25%',
+  address: '50%',
+});
+
+// 调整列宽的函数
+const adjustColumnWidths = () => {
+  const containerWidth = document.getElementById('myTable').offsetWidth - 20; // 获取表格容器的宽度
+  columnWidths.value = {
+    date: `${containerWidth * 0.25}px`, // 25%
+    name: `${containerWidth * 0.25}px`, // 25%
+    address: `${containerWidth * 0.5}px`, // 50%
+  };
+};
+
+// 在组件挂载后和窗口大小变化时调整列宽
+onMounted(() => {
+  window.addEventListener('resize', adjustColumnWidths);
+  adjustColumnWidths(); // 初始调整
+});
+
+// 在组件卸载前清理事件监听器
+onUnmounted(() => {
+  window.removeEventListener('resize', adjustColumnWidths);
+});
+</script>
--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import type { TabsPaneContext } from 'element-plus';
 import CheckStream from '@/components/tools/CheckStream.vue';
+import VerInfo from '@/components/tools/VerInfo.vue';
 
 const activeName = ref('first');
 
@@ -13,9 +14,9 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
 <template>
   <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
     <el-tab-pane label="Check Stream" name="first"><CheckStream /></el-tab-pane>
-    <el-tab-pane label="TODO" name="TODO...">Config</el-tab-pane>
-    <el-tab-pane label="TODO" name="TODO...">Role</el-tab-pane>
-    <el-tab-pane label="TODO" name="TODO...">Task</el-tab-pane>
+    <el-tab-pane label="Ver Info" name="TODO1"><VerInfo /></el-tab-pane>
+    <el-tab-pane label="TODO" name="TODO2">Role</el-tab-pane>
+    <el-tab-pane label="TODO" name="TODO3">Task</el-tab-pane>
   </el-tabs>
 </template>
 
